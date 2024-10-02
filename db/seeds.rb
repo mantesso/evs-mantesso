9d2b59c88db269c17f6c74febe726507e30bda0c
mantesso <fmantesso@gmail.com>
2024-10-02 10:19:06 +0200

    refactor seeds.rb to work with multiple dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-seed_date = Date.today
+def seed_potato_trades_for_date(seed_date)
+  trading_start_hour = 9
+  trading_end_hour = 17
 
-trading_start_hour = 9
-trading_end_hour = 17
+  (trading_start_hour..trading_end_hour).each do |hour|
+    time = seed_date.to_time.change(hour:)
+    value = rand(100.0..200.0).round(2)
 
-(trading_start_hour..trading_end_hour).each do |hour|
-  time = seed_date.to_time.change(hour:)
-  value = rand(100.0..200.0).round(2)
+    PotatoTrade.create(time:, value:)
+  end
 
-  PotatoTrade.create(time:, value:)
+  puts "Done seeding for #{seed_date}."
 end
 
-puts "done seeding for #{seed_date}."
+seed_potato_trades_for_date(Date.today)
+seed_potato_trades_for_date(Date.yesterday)
